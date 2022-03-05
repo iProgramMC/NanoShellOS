0420d9662427abfc2b66a655aab090cc39de6abf
czapek1337 <czapek1337@gmail.com>
2022-03-05 15:05:06 +0100

    Get rid of the unnecessary tools

--- clean.bat
+++
@@ -1,9 +0,0 @@
-@del /q build\*
-@del /q build\asm\*
-@del /q build\kapp\*
-@del /q build\fs\*
-@del /q build\optimiz\*
-@del /q build\icons\*
-@del /q kernel.bin
-@del /q nanoshell_initrd
-@echo Clean finished
