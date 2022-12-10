b76940e42a0985ecadae2416a88f18464dbeb0f0
iProgramMC <iprogramincpp@gmail.com>
2022-12-10 14:40:41 +0200

    * Separate the global namespace console stuff from the console manipulator functions.

--- src/co/base.c
+++ src/co/base.c
@@ -11,16 +11,12 @@
 #include <console.h>
 #include <video.h>
 
-#undef cli
-#define cli __asm__("cli\n\t")
-
-//note: important system calls are preceded by cli and succeeded by sti
-
 uint16_t TextModeMakeChar(uint8_t fgbg, uint8_t chr) {
 	uint8_t comb = fgbg;
 	uint16_t comb2 = comb << 8 | chr;
 	return comb2;
 }
+
 uint32_t g_vgaColorsToRGB[] = {
 	0x00000000,
 	0x000000AA,
@@ -39,6 +35,7 @@ uint32_t g_vgaColorsToRGB[] = {
 	0x00FFFF55,
 	0x00FFFFFF,
 };
+
 extern bool g_uses8by16Font;
 Console g_debugConsole; // for LogMsg
 Console g_debugSerialConsole; // for SLogMsg
@@ -546,6 +543,7 @@ void CoPrintChar (Console* this, char c)
 {
 	CoPrintCharInternal(this, c, '\0', true);
 }
+
 void CoPrintString (Console* this, const char *c)
 {
 	if (this->type == CONSOLE_TYPE_NONE) return; // Not Initialized
@@ -570,10 +568,12 @@ void CoAddToInputQueue (Console* this, char input)
 	   (this->m_inputBufferEnd >= KB_BUF_SIZE)
 		this->m_inputBufferEnd -= KB_BUF_SIZE;
 }
+
 bool CoAnythingOnInputQueue (Console* this)
 {
 	return this->m_inputBufferBeg != this->m_inputBufferEnd;
 }
+
 char CoReadFromInputQueue (Console* this)
 {
 	if (CoAnythingOnInputQueue(this))
@@ -585,157 +585,4 @@ char CoReadFromInputQueue (Console* this)
 		return k;
 	}
 	else return 0;
-}
-
-bool CoInputBufferEmpty()
-{
-	return !CoAnythingOnInputQueue (g_currentConsole);
-}
-extern void KeTaskDone();
-char CoGetChar()
-{
-	while (!CoAnythingOnInputQueue (g_currentConsole))
-		KeTaskDone();
-	return CoReadFromInputQueue (g_currentConsole);
-}
-void CoGetString(char* buffer, int max_size)
-{
-	int index = 0, max_length = max_size - 1;
-	//index represents where the next character we type would go
-	while (index < max_length)
-	{
-		//! has to stall
-		char k = CoGetChar();
-		if (k == '\n')
-		{
-			//return:
-			LogMsgNoCr("%c", k);
-			buffer[index++] = 0;
-			return;
-		}
-		else if (k == '\b')
-		{
-			if (index > 0)
-			{
-				LogMsgNoCr("%c", k);
-				index--;
-				buffer[index] = 0;
-			}
-		}
-		else
-		{
-			buffer[index++] = k;
-			LogMsgNoCr("%c", k);
-		}
-	}
-	LogMsg("");
-	buffer[index] = 0;
-}
-
-
-void CLogMsg (Console* pConsole, const char* fmt, ...) {
-	////allocate a buffer well sized
-	char cr[8192];
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(cr, fmt, list);
-	
-	sprintf (cr + strlen(cr), "\n");
-	CoPrintString(pConsole, cr);
-	
-	va_end(list);
-}
-void CLogMsgNoCr (Console* pConsole, const char* fmt, ...) {
-	////allocate a buffer well sized
-	char cr[8192];
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(cr, fmt, list);
-	CoPrintString(pConsole, cr);
-	
-	va_end(list);
-}
-void LogMsg (const char* fmt, ...) {
-	////allocate a buffer well sized
-	char cr[8192];
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(cr, fmt, list);
-	
-	sprintf (cr + strlen(cr), "\n");
-	CoPrintString(g_currentConsole, cr);
-	
-	va_end(list);
-}
-void LogMsgNoCr (const char* fmt, ...) {
-	////allocate a buffer well sized
-	char cr[8192];
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(cr, fmt, list);
-	CoPrintString(g_currentConsole, cr);
-	
-	va_end(list);
-}
-void SLogMsg (const char* fmt, ...) {
-	////allocate a buffer well sized
-	char cr[8192];
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(cr, fmt, list);
-	
-	sprintf (cr + strlen(cr), "\n");
-	CoPrintString(&g_debugSerialConsole, cr);
-	
-	va_end(list);
-}
-void SLogMsgNoCr (const char* fmt, ...) {
-	////allocate a buffer well sized
-	char cr[8192];
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(cr, fmt, list);
-	CoPrintString(&g_debugSerialConsole, cr);
-	
-	va_end(list);
-}
-const char* g_uppercaseHex = "0123456789ABCDEF";
-
-void LogHexDumpData (void* pData, int size) {
-	uint8_t* pDataAsNums = (uint8_t*)pData, *pDataAsText = (uint8_t*)pData;
-	char c[7], d[4];
-	c[5] = 0;   d[2] = ' ';
-	c[6] = ' '; d[3] = 0;
-	c[4] = ':';
-	
-	#define BYTES_PER_ROW 16
-	for (int i = 0; i < size; i += BYTES_PER_ROW) {
-		// print the offset
-		c[0] = g_uppercaseHex[(i & 0xF000) >> 12];
-		c[1] = g_uppercaseHex[(i & 0x0F00) >>  8];
-		c[2] = g_uppercaseHex[(i & 0x00F0) >>  4];
-		c[3] = g_uppercaseHex[(i & 0x000F) >>  0];
-		LogMsgNoCr("%s", c);
-		
-		for (int j = 0; j < BYTES_PER_ROW; j++) {
-			uint8_t p = *pDataAsNums++;
-			d[0] = g_uppercaseHex[(p & 0xF0) >> 4];
-			d[1] = g_uppercaseHex[(p & 0x0F) >> 0];
-			LogMsgNoCr("%s", d);
-		}
-		LogMsgNoCr("   ");
-		for (int j = 0; j < BYTES_PER_ROW; j++) {
-			char c = *pDataAsText++;
-			if (c < ' ') c = '.';
-			LogMsgNoCr("%c",c);
-		}
-		LogMsg("");
-	}
-}
-
-
-void CoKickOff()
-{
-	CoInitAsE9Hack(&g_debugConsole);
-	CoInitAsE9Hack(&g_debugSerialConsole);
 }
---
+++ src/console.c
@@ -0,0 +1,183 @@
+//  ***************************************************************
+//  console.c - Creation date: 09/12/2022
+//  -------------------------------------------------------------
+//  NanoShell Copyright (C) 2022 - Licensed under GPL V3
+//
+//  ***************************************************************
+//  Programmer(s):  iProgramInCpp (iprogramincpp@gmail.com)
+//  ***************************************************************
+
+#include <console.h>
+#include <string.h>
+#include <print.h>
+
+extern Console* g_currentConsole;
+
+bool CoInputBufferEmpty()
+{
+	return !CoAnythingOnInputQueue (g_currentConsole);
+}
+
+extern void KeTaskDone();
+
+char CoGetChar()
+{
+	while (!CoAnythingOnInputQueue (g_currentConsole))
+		KeTaskDone();
+	return CoReadFromInputQueue (g_currentConsole);
+}
+
+void CoKickOff()
+{
+	CoInitAsE9Hack(&g_debugConsole);
+	CoInitAsE9Hack(&g_debugSerialConsole);
+}
+
+void CoGetString(char* buffer, int max_size)
+{
+	int index = 0, max_length = max_size - 1;
+	//index represents where the next character we type would go
+	while (index < max_length)
+	{
+		//! has to stall
+		char k = CoGetChar();
+		if (k == '\n')
+		{
+			//return:
+			LogMsgNoCr("%c", k);
+			buffer[index++] = 0;
+			return;
+		}
+		else if (k == '\b')
+		{
+			if (index > 0)
+			{
+				LogMsgNoCr("%c", k);
+				index--;
+				buffer[index] = 0;
+			}
+		}
+		else
+		{
+			buffer[index++] = k;
+			LogMsgNoCr("%c", k);
+		}
+	}
+	LogMsg("");
+	buffer[index] = 0;
+}
+
+
+void CLogMsg (Console* pConsole, const char* fmt, ...)
+{
+	////allocate a buffer well sized
+	char cr[8192];
+	va_list list;
+	va_start(list, fmt);
+	vsprintf(cr, fmt, list);
+	
+	sprintf (cr + strlen(cr), "\n");
+	CoPrintString(pConsole, cr);
+	
+	va_end(list);
+}
+
+void CLogMsgNoCr (Console* pConsole, const char* fmt, ...)
+{
+	////allocate a buffer well sized
+	char cr[8192];
+	va_list list;
+	va_start(list, fmt);
+	vsprintf(cr, fmt, list);
+	CoPrintString(pConsole, cr);
+	
+	va_end(list);
+}
+
+void LogMsg (const char* fmt, ...)
+{
+	////allocate a buffer well sized
+	char cr[8192];
+	va_list list;
+	va_start(list, fmt);
+	vsprintf(cr, fmt, list);
+	
+	sprintf (cr + strlen(cr), "\n");
+	CoPrintString(g_currentConsole, cr);
+	
+	va_end(list);
+}
+
+void LogMsgNoCr (const char* fmt, ...)
+{
+	////allocate a buffer well sized
+	char cr[8192];
+	va_list list;
+	va_start(list, fmt);
+	vsprintf(cr, fmt, list);
+	CoPrintString(g_currentConsole, cr);
+	
+	va_end(list);
+}
+
+void SLogMsg (const char* fmt, ...){
+	////allocate a buffer well sized
+	char cr[8192];
+	va_list list;
+	va_start(list, fmt);
+	vsprintf(cr, fmt, list);
+	
+	sprintf (cr + strlen(cr), "\n");
+	CoPrintString(&g_debugSerialConsole, cr);
+	
+	va_end(list);
+}
+
+void SLogMsgNoCr (const char* fmt, ...)
+{
+	////allocate a buffer well sized
+	char cr[8192];
+	va_list list;
+	va_start(list, fmt);
+	vsprintf(cr, fmt, list);
+	CoPrintString(&g_debugSerialConsole, cr);
+	
+	va_end(list);
+}
+
+const char* g_uppercaseHex = "0123456789ABCDEF";
+
+void LogHexDumpData (void* pData, int size)
+{
+	uint8_t* pDataAsNums = (uint8_t*)pData, *pDataAsText = (uint8_t*)pData;
+	char c[7], d[4];
+	c[5] = 0;   d[2] = ' ';
+	c[6] = ' '; d[3] = 0;
+	c[4] = ':';
+	
+	#define BYTES_PER_ROW 16
+	for (int i = 0; i < size; i += BYTES_PER_ROW) {
+		// print the offset
+		c[0] = g_uppercaseHex[(i & 0xF000) >> 12];
+		c[1] = g_uppercaseHex[(i & 0x0F00) >>  8];
+		c[2] = g_uppercaseHex[(i & 0x00F0) >>  4];
+		c[3] = g_uppercaseHex[(i & 0x000F) >>  0];
+		LogMsgNoCr("%s", c);
+		
+		for (int j = 0; j < BYTES_PER_ROW; j++) {
+			uint8_t p = *pDataAsNums++;
+			d[0] = g_uppercaseHex[(p & 0xF0) >> 4];
+			d[1] = g_uppercaseHex[(p & 0x0F) >> 0];
+			LogMsgNoCr("%s", d);
+		}
+		LogMsgNoCr("   ");
+		for (int j = 0; j < BYTES_PER_ROW; j++) {
+			char c = *pDataAsText++;
+			if (c < ' ') c = '.';
+			LogMsgNoCr("%c",c);
+		}
+		LogMsg("");
+	}
+}
+
+
