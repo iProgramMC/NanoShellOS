4d27fe66ee987bbb589be8d0d360bf7e40f7b7ac
iProgramMC <iprogramincpp@gmail.com>
2023-02-12 10:03:00 +0200

    * Re-add the crash handler

--- src/idt.c
+++ src/idt.c
@@ -156,7 +156,7 @@ void IsrExceptionCommon(int code, Registers* pRegs)
 	VidSetFont (FONT_TAMSYN_BOLD);
 	
 	//If we're running a task:
-	if (false && KeGetRunningTask() != NULL)
+	if (KeGetRunningTask() != NULL)
 	{
 		if (!g_hasAlreadyThrownException1)
 		{
