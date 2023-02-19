1ae914018e486c91e5e31bb1bed36a6270aab875
iProgramMC <iprogramincpp@gmail.com>
2023-02-19 17:44:09 +0200

    * Rename the multibootdata and text section to IPL data and text.

--- src/asm/earlybird.asm
+++ src/asm/earlybird.asm
@@ -6,80 +6,15 @@
 ;
 bits 32
 
-;%define DONT_ENABLE_FPU
-
-; Setup kernel page directory and subsequent pages.  Maps the entire memory map from 0x00000000 to BASE_ADDRESS.
-%define BASE_ADDRESS 0xC0000000
-%define VIRT_TO_PHYS(k) ((k) - BASE_ADDRESS)
-	
-; variables to load from other c/asm files
-extern g_KernelPageDirectory
-extern g_pageTableArray
-extern e_placement
-extern e_frameBitsetSize
-extern e_frameBitsetVirt
-extern e_temporary1
-extern e_temporary2
-
 ; functions to load from other c/asm files
 extern KiStartupSystem
 
-; things that aren't static and exclusive to this object file 
-global KeEntry
+extern g_KernelPageDirectory
+extern e_temporary1
+extern e_temporary2
 
-section .multiboottext
-
-KeEntry:
-	cli					; Block all interrupts.  We don't have them setup yet
-	
-	; We don't actually need a stack at this stage
-	xor ebp, ebp
-	
-	mov [VIRT_TO_PHYS(e_temporary1)], eax	; Make a backup of the multiboot parameters.
-	mov [VIRT_TO_PHYS(e_temporary2)], ebx
-	
-	; First address to map is 0x00000000
-	xor esi, esi
-
-	; Map 8192 pages.
-	mov ecx, 8192
-	
-	; Get physical address of the boot page table.
-	mov edi, VIRT_TO_PHYS (g_pageTableArray)
-	
-.label1:
-	mov edx, esi
-	or  edx, 0x03 ; Set present and R/W bits
-	mov [edi], edx
-	
-	add esi, 0x1000 ; The size of a page is 4096 bytes
-	add edi, 4
-	;inc dword [VIRT_TO_PHYS(e_pageTableNum)]
-	loop .label1
-	
-	; NOTE: If the kernel goes haywire just change PagesToMap
-.label3:
-	
-	; Map the two pagetables required to both virtual addresses 0x0 and 0xC0000000
-	mov dword [VIRT_TO_PHYS(g_KernelPageDirectory) +   0*4], VIRT_TO_PHYS(g_pageTableArray+0000) + 0x03
-	mov dword [VIRT_TO_PHYS(g_KernelPageDirectory) +   1*4], VIRT_TO_PHYS(g_pageTableArray+4096) + 0x03
-	mov dword [VIRT_TO_PHYS(g_KernelPageDirectory) + 768*4], VIRT_TO_PHYS(g_pageTableArray+0000) + 0x03
-	mov dword [VIRT_TO_PHYS(g_KernelPageDirectory) + 769*4], VIRT_TO_PHYS(g_pageTableArray+4096) + 0x03
-	
-	; Set CR3 to the physical address of the g_KernelPageDirectory
-	mov ecx, VIRT_TO_PHYS(g_KernelPageDirectory)
-	mov cr3, ecx
-	
-	; Set PG and WP bit
-	mov ecx, cr0
-	or  ecx, 0x80010000
-	mov cr0, ecx
-	
-	; Jump to higher half:
-	mov ecx, (KeHigherHalfEntry)
-	jmp ecx
-	
 section .text
+global KeHigherHalfEntry
 KeHigherHalfEntry:
 	; Unmap the identity mapping, we don't need it anymore
 	mov dword [g_KernelPageDirectory+0], 0
